de4f92d5d9bad7bb08ffb46977fe0824d81237f5
pavle_j <skydevhawkz@gmail.com>
2026-03-15 14:27:30 +0100

    Added base.css, enqueue.php and functions.php, also added fonts

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+require_once get_template_directory() . '/inc/enqueue.php';
+
+
+// Theme setup
+// require_once get_template_directory() . '/inc/setup.php';
+
+// Helpers
+// require_once get_template_directory() . '/inc/helpers.php';
+
+// WooCommerce
+// require_once get_template_directory() . '/inc/woocommerce.php';
+
+// SEO / Schema
+// require_once get_template_directory() . '/inc/schema.php';
+
+// Performance
+// require_once get_template_directory() . '/inc/performance.php';
+
+// Security
+// require_once get_template_directory() . '/inc/security.php';
---
+++ inc/enqueue.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+function tersa_enqueue_assets() {
+	$theme_version = wp_get_theme()->get('Version');
+
+	$theme_uri = get_template_directory_uri();
+
+	wp_enqueue_style('tersa-base', $theme_uri . '/assets/css/base.css', [], $theme_version);
+	/*
+	wp_enqueue_style('tersa-layout', $theme_uri . '/assets/css/layout.css', ['tersa-base'], $theme_version);
+	wp_enqueue_style('tersa-header', $theme_uri . '/assets/css/header.css', ['tersa-base'], $theme_version);
+	wp_enqueue_style('tersa-footer', $theme_uri . '/assets/css/footer.css', ['tersa-base'], $theme_version);
+
+	if (is_front_page()) {
+		wp_enqueue_style('tersa-home', $theme_uri . '/assets/css/home.css', ['tersa-base', 'tersa-layout'], $theme_version);
+	}
+
+	if (function_exists('is_shop') && (is_shop() || is_product_category())) {
+		wp_enqueue_style('tersa-shop', $theme_uri . '/assets/css/shop.css', ['tersa-base', 'tersa-layout'], $theme_version);
+	}
+
+	if (function_exists('is_product') && is_product()) {
+		wp_enqueue_style('tersa-product', $theme_uri . '/assets/css/product.css', ['tersa-base', 'tersa-layout'], $theme_version);
+	}
+
+	if (function_exists('is_cart') && is_cart()) {
+		wp_enqueue_style('tersa-cart', $theme_uri . '/assets/css/cart.css', ['tersa-base', 'tersa-layout'], $theme_version);
+	}
+
+	if (function_exists('is_checkout') && is_checkout()) {
+		wp_enqueue_style('tersa-checkout', $theme_uri . '/assets/css/checkout.css', ['tersa-base', 'tersa-layout'], $theme_version);
+	}
+	*/
+}
+add_action('wp_enqueue_scripts', 'tersa_enqueue_assets');
